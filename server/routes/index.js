251b4deba6ff108ccbe740806bb6f8c1d27edb8d
carolineminassian <31682416+carolineminassian@users.noreply.github.com>
2021-11-12 02:01:54 +0100

    setting up

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const routeGuard = require('./../middleware/route-guard');
 
 router.get('/', (req, res, next) => {
-  res.json({ type: 'success', data: { title: 'Hello World' } });
+  res.json({ type: 'success!', data: { title: 'Hello Server' } });
 });
 
 router.get('/private', routeGuard, (req, res, next) => {
