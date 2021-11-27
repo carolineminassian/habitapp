d79046c0e08f5c894c1966516dc94b341defe7a8
carolineminassian <31682416+carolineminassian@users.noreply.github.com>
2021-11-27 02:25:14 +0100

    deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3010',
+  baseURL: process.env.REACT_APP_API_URL,
+  // 'http://localhost:3010',
   // baseURL: process.env.REACT_APP_API_URL, // in .env file: REACT_APP_API_URL=https:://ironhack-habitapp.herokuapp.com
   withCredentials: true
 });
